0363f02508bce1d6da94193f0cb3582e2b5d70ad
monisha2504 <71072467+monisha2504@users.noreply.github.com>
2021-04-04 13:20:33 +0530

    Create Person.java

---
+++ Lab4/Person.java
@@ -0,0 +1,25 @@
+package Lab4;
+
+public class Person {
+	private String name;
+	private float age;
+	Person(){}
+	Person(String name,float age)
+	{
+		this.name=name;
+		this.age=age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getAge() {
+		return age;
+	}
+	public void setAge(float age) {
+		this.age = age;
+	}
+
+}
