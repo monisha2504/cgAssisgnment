31dee919784a678d57951c24ce29fc03633cc9da
monisha2504 <71072467+monisha2504@users.noreply.github.com>
2021-04-04 13:17:48 +0530

    Create Alphabet.java

---
+++ Pkg/Alphabet.java
@@ -0,0 +1,19 @@
+package Pkg;
+
+public class Alphabet {
+
+	public static void main(String[] args) {
+		char ch='b';
+		if(ch == 'a'|| ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+		{
+			System.out.println("is a vowel");
+			
+		}
+		else
+		{
+			System.out.println("not a vowel");
+		}
+
+	}
+
+}
