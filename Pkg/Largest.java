bc7d6bbdabffa2279d10a553177201d1a356dde8
monisha2504 <71072467+monisha2504@users.noreply.github.com>
2021-04-04 13:18:23 +0530

    Add files via upload

---
+++ Pkg/Largest.java
@@ -0,0 +1,22 @@
+package Pkg;
+
+public class Largest {
+	public static void main(String[] args) {
+		int num1=30;
+		int num2=70;
+		int num3=10;
+		if(num1>num2 && num1>num3)
+		{
+			System.out.println("num1 is greater");
+		}
+		else if(num2>num3)
+		{
+			System.out.println("num2 is greater");
+			
+		}
+		else
+		{
+			System.out.println("num3 is greater");
+		}
+	}
+}
---
+++ Pkg/Swap.java
@@ -0,0 +1,14 @@
+package Pkg;
+
+public class Swap {
+	public static void main(String[] args) {
+		int a=10;
+		int b=40;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
