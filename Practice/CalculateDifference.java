6a660d1702d7004b37584c63c0adc1636dd55394
monisha2504 <71072467+monisha2504@users.noreply.github.com>
2021-04-04 13:16:37 +0530

    Create CalculateDifference.java

---
+++ Practice/CalculateDifference.java
@@ -0,0 +1,21 @@
+package Practice;
+
+public class CalculateDifference {
+	int Square(int n) {
+		int squarenum=(n*(n+1)+(2*(n+1)))/6;
+		int num1=(n*(n+1))/2;
+		int squares=(num1)^2;
+		System.out.println(squares);
+		int difference=(squarenum-squares);
+		return difference;
+		
+	}
+		
+		public static void main(String[] args) {
+		CalculateDifference cd=new CalculateDifference();
+		System.out.println(cd.Square(10));
+	}
+}
+
+
+	
